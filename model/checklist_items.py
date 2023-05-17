186dabc5ca094b3f4a7d965d570d3de9cc86fda6
Tangkas Mukti Widodo <57988300+frostmomo@users.noreply.github.com>
2023-05-17 19:10:00 +0700

    Update checklist_items.py
    Updated responses to serialize

--- model/checklist_items.py
+++ model/checklist_items.py
@@ -1,4 +1,5 @@
 import datetime
+from sqlalchemy.orm import relationship
 from app import db
 from sqlalchemy import ForeignKey
 
@@ -8,19 +9,24 @@ class ChecklistItems(db.Model):
     id = db.Column(db.Integer(),primary_key=True, autoincrement=True)
     checklist_id = db.Column(db.Integer(), ForeignKey("checklists.id"))
     name = db.Column(db.String())
+    status = db.Column(db.String(), default="Deactivated")
     
     created_at = db.Column(db.TIMESTAMP(), nullable=True, default=datetime.datetime.utcnow())
     updated_at = db.Column(db.TIMESTAMP(), nullable=True, default=datetime.datetime.utcnow())
     deleted_at = db.Column(db.TIMESTAMP(), nullable=True)
+
+    checklist = relationship("Checklists", backref="checklist_items", uselist=False)
     
     def __init__(
         self,
+        checklist_id,
         name,
         deleted_at,
         created_at,
         updated_at,
     ):
         self.name = name
+        self.checklist_id = checklist_id
         self.deleted_at = deleted_at
         self.created_at = created_at
         self.updated_at = updated_at
@@ -31,5 +37,10 @@ class ChecklistItems(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "name": self.name
+            "name": self.name,
+            "checklist":{
+                "id":self.checklist.id if self.checklist is not None else None,
+                "name":self.checklist.name if self.checklist is not None else None
+            },
+            "status": self.status
         }
