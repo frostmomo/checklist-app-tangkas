89ed288472d1fde374a3adffe2906f5b13e9c07c
Tangkas Mukti Widodo <57988300+frostmomo@users.noreply.github.com>
2023-05-17 19:09:28 +0700

    Update checklist_item_repository.py
    Updated some repository

--- repository/checklist_item_repository.py
+++ repository/checklist_item_repository.py
@@ -2,22 +2,19 @@ from app import db
 
 from model.checklist_items import ChecklistItems
 
-
+ 
 class ChecklistItemRepository(db.Model):
     __tablename__ = "checklist_items"
+
+    def get_all_by_checklist_id(checklist_id):
+        data = ChecklistItems.query.filter_by(checklist_id=checklist_id, deleted_at=None).all()
+        return data     
 
     def get_by_id(id):
         data = ChecklistItems.query.filter_by(id=id, deleted_at=None).first()
         return data
-
-    def get_by_email(emailaccount):
-        data = ChecklistItems.query.filter_by(email=emailaccount).first()
-        return data
-  
-    def get_by_phone(dataPhone):
-        data = ChecklistItems.query.filter_by(phoneNumber=dataPhone).first()
+    
+    def get_by_checklist_id(id,checklist_id):
+        data = ChecklistItems.query.filter_by(id=id,checklist_id=checklist_id, deleted_at=None).first()
         return data
 
-    def get_by_client_id(dataClientId):
-        data = ChecklistItems.query.filter_by(client_id=dataClientId).all()
-        return data
